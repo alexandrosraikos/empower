62e030cf3c3e572153042bb7df67e287fe4797be
Αλέξανδρος Ράικος <alexandros@pop-os.localdomain>
2020-10-06 10:44:55 +0300

    made skeleton

--- storefront/src/App.js
+++ storefront/src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="App">
+      <blockquote>
+      What is better? To be born good or to overcome your evil nature through great effort?
+      </blockquote>
+      <cite>Paarthurnax</cite>
+      <button>I need another</button>
+    </section>
   );
 }
 
--- storefront/src/index.js
+++ storefront/src/index.js
@@ -6,7 +6,9 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
+    <header><a href="/"><h1>Empower</h1></a></header>
     <App />
+    <footer>Made by <a href="https://www.araikos.gr/" target="blank">A. Raikos</a> in <a href="http://www.kep.unipi.gr" target="blank">UPRC</a>.</footer>
   </React.StrictMode>,
   document.getElementById('root')
 );
