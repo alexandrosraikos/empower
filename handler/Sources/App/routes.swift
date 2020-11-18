1a16be9ce0389d724894bf736d040e29f18476fc
Alexandros Raikos <dev@araikos.gr>
2020-11-18 08:32:55 +0000

    changes

--- handler/Sources/App/routes.swift
+++ handler/Sources/App/routes.swift
@@ -6,7 +6,7 @@ import Prometheus
 func routes(_ app: Application) throws {
     
     // ---- Session keeping.
-    let sessions = app.grouped(app.sessions.middleware)
+    _ = app.grouped(app.sessions.middleware)
     
     // ---- Initialize Prometheus exporting &  metrics.
     let prometheusClient = PrometheusClient()
@@ -16,18 +16,21 @@ func routes(_ app: Application) throws {
     // -- METRIC: Response time for each database request.
     var dbRequestTime = Date()
     let dbResponseTime = prometheusClient.createGauge(forType: Int.self, named: "database_response_time")
+
+    // -- METRIC: Requests routed between metric scrapes.
+    let numberOfRequests = prometheusClient.createGauge(forType: Int.self, named: "total_requests")
     
     // -- METRIC: Active sessions.
     let activeUserCount = prometheusClient.createGauge(forType: Int.self, named: "recently_active_users")
-    
-    // -- METRIC: Requests routed between metric scrapes.
-    let numberOfRequests = prometheusClient.createGauge(forType: Int.self, named: "total_requests")
     
     // Route for the front-end web application.
     app.get("quotes") { req -> EventLoopFuture<Quote> in
         numberOfRequests.inc()
         dbRequestTime = Date()
-    
+        
+        // Capture session.
+        req.session.data["uuid"] = req.parameters.get("uuid")
+        
         // Count RTT for Mongo request.
         let quote = Quote
             .query(on: app.db)
@@ -47,9 +50,7 @@ func routes(_ app: Application) throws {
     
     // Route for the Netdata Prometheus collector.
     app.get("metrics") { req -> EventLoopFuture<String> in
-        // -- Exposing active session count.
         activeUserCount.set(app.sessions.memory.storage.sessions.count)
-        
         let metricResponse = req.eventLoop.makePromise(of: String.self)
         metricsCollector.collect(into: metricResponse)
         return metricResponse.futureResult
